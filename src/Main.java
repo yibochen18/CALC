3b5229f9314f979c1345840e068fbcad2ec589b4
Yibo Chen <yibochen2019@gmail.com>
2022-01-15 17:53:41 -0700

    made main java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,27 @@
-public class Main {
+import java.util.Scanner;
+import java.util.*;
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of courses you have: ");
+        int num_course = sc.nextInt();
+        ArrayList<Course> courses = new ArrayList<Course>();
+
+        for(int i=0; i<num_course; i++) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Course name: ");
+            String name = input.nextLine();
+            System.out.println("How many credits is the course: ");
+            int credits = input.nextInt();
+            System.out.println("Start time (round up to the nearest hour on 24 hour clock): ");
+            int start = input.nextInt();
+            System.out.println("End time (round up to the nearest hour on 24 hour clock): ");
+            int end = input.nextInt();
+            Course newCourse = new Course(name,credits,start,end);
+            courses.add(newCourse);
+        }
+    }
 }
+
